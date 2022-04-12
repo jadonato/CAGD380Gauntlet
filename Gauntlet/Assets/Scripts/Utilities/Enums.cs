01b04b18e85c7d460ee7a0352a9154b89baa90de
jadonato <jadonato@mail.csuchico.edu>
2022-04-12 11:52:35 -0700

    Setup some script files for later

---
+++ Gauntlet/Assets/Scripts/Utilities/Enums.cs
@@ -0,0 +1,22 @@
+public enum ClassType
+{
+    Warrior,
+    Valkyrie,
+    Wizard,
+    Elf
+}
+
+public enum ItemType
+{
+    BluePotion,
+    OrangePotion
+}
+
+public enum Stats
+{
+    Health,
+    MeeleDamage,
+    MagicDamage,
+    Defense,
+    MoveSpeed
+}
