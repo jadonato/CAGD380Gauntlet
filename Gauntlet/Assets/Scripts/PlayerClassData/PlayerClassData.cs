4bd12119715cad11301f2e6d8cde5bbaa97c27eb
jadonato <jadonato@mail.csuchico.edu>
2022-04-14 11:49:31 -0700

    Added player input actions

--- Gauntlet/Assets/Scripts/PlayerClassData/PlayerClassData.cs
+++ Gauntlet/Assets/Scripts/PlayerClassData/PlayerClassData.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "NewPlayerClassData", menuName = "PlayerClassData", order = 51)]
 public class PlayerClassData : ScriptableObject
 {
+    [SerializeField] private Sprite _sprite;
     [SerializeField] private float _baseHealth;
     [SerializeField] private float _meleeDamage;
     [SerializeField] private float _magicDamage;
     [SerializeField] private float _defense;
     [SerializeField] private float _moveSpeed;
 
+    public Sprite Sprite { get { return _sprite; } }
     public float BaseHealth { get { return _baseHealth; } }
     public float MeeleDamage { get { return _meleeDamage; } }
     public float MagicDamage { get { return _magicDamage; } }
